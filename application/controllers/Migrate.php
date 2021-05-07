9a62833685c05705645ad59877c629e813cd8dcd
Edgar Eldy <ed.eldy21@live.com>
2021-05-07 23:09:05 +0200

    Add Migrate Controller for managing database migrations

---
+++ application/controllers/Migrate.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migrate extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+
+    public function index()
+    {
+    }
+}
+
+/* End of file Migrate.php */

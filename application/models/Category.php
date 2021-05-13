da541797d9eac32c099740acfa8cc360e2271969
Edgar Eldy <ed.eldy21@live.com>
2021-05-13 22:50:21 +0200

    Add a query for getting all product categories

--- application/models/Category.php
+++ application/models/Category.php
@@ -11,6 +11,19 @@ class Category extends CI_Model
         parent::__construct();
         //Do your magic here
     }
+
+    // Get all product categories
+    public function getCategories()
+    {
+        $this->db->select('*');
+        $this->db->from('categories');
+        $query = $this->db->get();
+        
+        return $query->result_array();
+        
+    }
+
+
 }
 
 /* End of file Category.php */

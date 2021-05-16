d4e06dd529c7e8e1e15e51e8b709d82c29986c94
Edgar Eldy <ed.eldy21@live.com>
2021-05-16 23:17:01 +0200

    Add Product Model

---
+++ application/models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Product extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+}
+
+/* End of file Product.php */

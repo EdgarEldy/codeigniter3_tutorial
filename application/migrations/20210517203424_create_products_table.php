551abd861f759b9acc698fdcb7696b0316b276cd
Edgar Eldy <ed.eldy21@live.com>
2021-05-17 22:34:42 +0200

    Add products migration

---
+++ application/migrations/20210517203424_create_products_table.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_products_table extends CI_Migration
+{
+
+public function up()
+{
+
+}
+
+public function down()
+{
+
+}
+}

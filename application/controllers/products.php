1261bbeed4a3f7ed4219a36cc3dfad37fcd01a81
Edgar Eldy <ed.eldy21@live.com>
2021-06-02 22:59:06 +0200

    Getting products with their categories and render to the products view

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -2,19 +2,24 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class products extends CI_Controller
+class Products extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Product');
+        $this->load->model('Category');
+        
         
         //Do your magic here
     }
 
 
+    //Getting products with their categories
     public function index()
     {
+        $data['products'] = $this->Product->getProducts();
+        return $this->render('templates/default', 'content', 'products/index', $data);
     }
 }
 

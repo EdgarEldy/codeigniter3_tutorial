ebe86208b918f11f32784fd3532c6613a6e8b626
Edgar Eldy <ed.eldy21@live.com>
2021-06-02 22:58:35 +0200

    Add a query for getting products and their categories

--- application/models/Product.php
+++ application/models/Product.php
@@ -9,6 +9,18 @@ class Product extends CI_Model
         parent::__construct();
         //Do your magic here
     }
+
+    //Getting products with their categories query
+    public function getProducts()
+    {
+        $this->db->select('*');
+        $this->db->from('categories');
+        $this->db->join('products', 'categories.id = products.category_id', 'inner');
+        $query = $this->db->get();
+        $result = $query->result_array();
+
+        return !empty($result) ? $result : false;
+    }
 }
 
 /* End of file Product.php */

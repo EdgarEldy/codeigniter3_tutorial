036c1f85a0b1c794a4b7b1802a76c241d6ea449e
Edgar Eldy <ed.eldy21@live.com>
2021-06-03 11:28:58 +0200

    Add a function for getting products/add view and save a new product

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -21,6 +21,32 @@ class Products extends CI_Controller
         $data['products'] = $this->Product->getProducts();
         return $this->render('templates/default', 'content', 'products/index', $data);
     }
+
+    //Add a product
+    public function add()
+    {
+        //Getting categories
+        $data['categories'] = $this->Category->getCategories();
+
+        //Setting validation rules
+        $this->form_validation->set_rules('category_id', 'Category id', 'required');
+        $this->form_validation->set_rules('product_name', 'Product name', 'required');
+        $this->form_validation->set_rules('unit_price', 'Unit price', 'required');
+
+
+        if ($this->form_validation->run() == FALSE) {
+            return $this->render('templates/default', 'content', 'products/add', $data);
+        } else {
+            //Save product category
+            $this->Product->add();
+
+            //Set flash message
+            $this->session->set_flashdata('product_saved', 'Product has been saved successfully !');
+
+            //Load categories/index view
+            return redirect('products');
+        }
+    }
 }
 
 /* End of file products.php */

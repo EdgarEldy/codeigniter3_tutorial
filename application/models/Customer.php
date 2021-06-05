2908385587786c910deaa59ac9c52e79eea4ff0d
Edgar Eldy <ed.eldy21@live.com>
2021-06-05 23:44:00 +0200

    Add Customer Model

---
+++ application/models/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Customer extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+}
+
+/* End of file Customer.php */

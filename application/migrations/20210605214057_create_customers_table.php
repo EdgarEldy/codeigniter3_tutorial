5e571348eafda5ce4b9e262a98907d9b191916e8
Edgar Eldy <ed.eldy21@live.com>
2021-06-09 23:31:02 +0200

    Update customers migration : Add columns

--- application/migrations/20210605214057_create_customers_table.php
+++ application/migrations/20210605214057_create_customers_table.php
@@ -6,11 +6,48 @@ class Migration_Create_customers_table extends CI_Migration
 
 public function up()
 {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'first_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'last_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'tel' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'address' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'created_at' => array(
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+            ),
+            'modified_at' => array(
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+            ),
 
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('customers');
 }
 
 public function down()
 {
+        $this->dbforge->drop_table('customers');
 
 }
 }

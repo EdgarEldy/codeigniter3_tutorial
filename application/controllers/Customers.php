f46c3994767cf8165b8ebef00a44bb4a74ad73e6
Edgar Eldy <ed.eldy21@live.com>
2021-06-13 21:08:30 +0200

    Add a new customer with validations

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -19,6 +19,32 @@ class Customers extends CI_Controller {
         $data['customers'] = $this->Customer->getCustomers();
         return $this->render('templates/default', 'content', 'customers/index', $data);
     }
+
+    //Add a new customer
+    public function add()
+    {
+        //Setting validation rules
+        $this->form_validation->set_rules('first_name', 'First name', 'required');
+        $this->form_validation->set_rules('last_name', 'Last name', 'required');
+        $this->form_validation->set_rules('tel', 'Tel', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('address', 'Address', 'required');
+
+
+        if ($this->form_validation->run() == FALSE) {
+            return $this->render('templates/default', 'content', 'customers/add');
+        } else {
+
+            //Save a customer
+            $this->Customer->add();
+
+            //Set flash message
+            $this->session->set_flashdata('customer_saved', 'Customer has been saved successfully !');
+
+            //Load customers/index view
+            return redirect('customers');
+        }
+    }
 }
 
 /* End of file Customers.php */

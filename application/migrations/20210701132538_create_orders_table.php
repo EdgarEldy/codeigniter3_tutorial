1a9cfff3d68aca57d8f4ccba5f6f64709221180e
Edgar Eldy <ed.eldy21@live.com>
2021-07-01 15:31:42 +0200

    Update orders migration : Add columns

--- application/migrations/20210701132538_create_orders_table.php
+++ application/migrations/20210701132538_create_orders_table.php
@@ -4,13 +4,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Migration_Create_orders_table extends CI_Migration
 {
 
-public function up()
-{
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'customer_id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ), 
+            'product_id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ),       
+            'qty' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ),
+            'grand_total' => array(
+                'type' => 'FLOAT',
+            ),
+            'created_at' => array(
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+            ),
+            'modified_at' => array(
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+            ),
 
-}
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('orders');
+    }
 
-public function down()
-{
-
-}
+    public function down()
+    {
+        $this->dbforge->drop_table('orders');
+    }
 }

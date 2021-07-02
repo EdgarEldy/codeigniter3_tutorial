f16c2b7bd0c3d2585d00971d13fc8f5ecae54faf
Edgar Eldy <ed.eldy21@live.com>
2021-07-02 16:06:40 +0200

    Add Orders controller

---
+++ application/controllers/Orders.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Orders extends CI_Controller
+{
+    
+    //Constructor
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+    
+
+    public function index()
+    {
+    }
+}
+
+/* End of file Orders.php */

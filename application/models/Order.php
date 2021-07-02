4cecc9580a9336ce22b80ec9b81866b98c77a2ad
Edgar Eldy <ed.eldy21@live.com>
2021-07-02 16:05:01 +0200

    Add Order Model

---
+++ application/models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Order extends CI_Model
+{
+
+    //Constructor
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+}
+
+/* End of file Order.php */

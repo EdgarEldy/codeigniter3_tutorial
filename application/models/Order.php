33289700c1479cbf63e9bc16074aec4a629129f4
Edgar Eldy <ed.eldy21@live.com>
2021-07-03 12:20:57 +0200

    Get orders by customer

--- application/models/Order.php
+++ application/models/Order.php
@@ -11,6 +11,20 @@ class Order extends CI_Model
         parent::__construct();
         //Do your magic here
     }
+
+    //Get orders by customer
+    public function getOrders()
+    {
+        $this->db->select('*');
+        $this->db->from('orders');
+        $this->db->join('customers', 'orders.customer_id = customers.id', 'inner');
+        $this->db->join('products', 'orders.product_id = products.id', 'inner');
+        $query = $this->db->get();
+        
+        $result = $query->result_array();
+        
+        return !empty($result) ? $result : false;
+    }
 }
 
 /* End of file Order.php */

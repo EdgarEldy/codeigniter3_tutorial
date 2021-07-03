d016ece11ba9525b3ae7f91a9902ee17a406bba8
Edgar Eldy <ed.eldy21@live.com>
2021-07-03 12:21:36 +0200

    Add a function for showing orders

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Orders extends CI_Controller
 {
-    
+
     //Constructor
     public function __construct()
     {
@@ -12,13 +12,16 @@ class Orders extends CI_Controller
         $this->load->model('Order');
         $this->load->model('Customer');
         $this->load->model('Product');
-        
+
         //Do your magic here
     }
-    
 
+
+    //Show order
     public function index()
     {
+        $data['orders'] = $this->Order->getOrders();
+        return $this->render('templates/default', 'content', 'orders/index', $data);
     }
 }
 

f2b1f87421efabde049f3279d00707756805da87
Edgar Eldy <ed.eldy21@live.com>
2021-07-07 12:44:26 +0200

    Add order query

--- application/models/Order.php
+++ application/models/Order.php
@@ -20,10 +20,25 @@ class Order extends CI_Model
         $this->db->join('customers', 'orders.customer_id = customers.id', 'inner');
         $this->db->join('products', 'orders.product_id = products.id', 'inner');
         $query = $this->db->get();
-        
+
         $result = $query->result_array();
-        
+
         return !empty($result) ? $result : false;
+    }
+
+    //Add order query
+    public function add()
+    {
+        //Order data array
+        $data = array(
+            'customer_id' => $this->input->post('customer_id'),
+            'product_id' => $this->input->post('product_id'),
+            'qty' => $this->input->post('qty'),
+            'grand_total' => $this->input->post('grand_total'),
+        );
+
+        //Insert order
+        return $this->db->insert('orders', $data);
     }
 }
 

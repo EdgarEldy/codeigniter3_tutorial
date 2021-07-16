47bd0cb345bfea031e7c9579f46a1b4e64012dfa
Edgar Eldy <ed.eldy21@live.com>
2021-07-16 12:29:02 +0200

    Update order

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -13,7 +13,7 @@ class Orders extends CI_Controller
         $this->load->model('Customer');
         $this->load->model('Product');
         $this->load->model('Category');
-        
+
 
         //Do your magic here
     }
@@ -85,8 +85,35 @@ class Orders extends CI_Controller
         $data['customer'] = $this->Order->getCustomerByOrderId($id); //get customer by order id
         $data['product'] = $this->Order->getProductByOrderId($id); //Get product name by order id
         $data['category'] = $this->Order->getCategoryByOrderId($id); //Get category name by order id
-        $data['order'] = $this->Order->edit($id); //Get order data 
+        $data['order'] = $this->Order->edit($id); //Get order data
         $this->render('templates/default', 'content', 'orders/edit', $data);
+    }
+
+    //Update order
+    public function update()
+    {
+
+        //Setting validation rules
+        $this->form_validation->set_rules('customer_id', 'Customer id', 'required');
+        $this->form_validation->set_rules('category_id', 'Category id', 'required');
+        $this->form_validation->set_rules('product_id', 'Product id', 'required');
+        $this->form_validation->set_rules('qty', 'Quantity', 'required');
+        $this->form_validation->set_rules('unit_price', 'Unit price', 'required');
+        $this->form_validation->set_rules('grand_total', 'Total', 'required');
+
+
+        if ($this->form_validation->run() == FALSE) {
+            return $this->render('templates/default', 'content', 'orders/edit');
+        } else {
+            //Update order
+            $this->Order->Update();
+
+            //Set flash message
+            $this->session->set_flashdata('order_updated', 'Order has been updated successfully !');
+
+            //Load orders/index view
+            return redirect('orders');
+        }
     }
 }
 

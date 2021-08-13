d5780e3faf36571eaf2319268f72d9cd6cb1a2f1
Edgar Eldy <ed.eldy21@live.com>
2021-08-13 09:25:27 +0200

    Add Role Model

---
+++ application/models/Role.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Role extends CI_Model {
+
+//Constructor
+public function __construct()
+{
+    parent::__construct();
+    //Do your magic here
+}
+
+}
+
+/* End of file Role.php */

3aa3b6569dba9e053ebd5a682fb7486f0034f00e
Edgar Eldy <ed.eldy21@live.com>
2021-08-13 09:28:32 +0200

    Add Roles controller

---
+++ application/controllers/Roles.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Roles extends CI_Controller {
+
+    //Constructor
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+    
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Roles.php */

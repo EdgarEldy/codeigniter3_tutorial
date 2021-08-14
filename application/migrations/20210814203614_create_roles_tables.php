a691dd7dd6c049ff3ff7578b36b53cf1fcfaeb34
Edgar Eldy <ed.eldy21@live.com>
2021-08-14 22:38:57 +0200

    Update roles migration : Add columns

--- application/migrations/20210814203614_create_roles_tables.php
+++ application/migrations/20210814203614_create_roles_tables.php
@@ -1,16 +1,36 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Migration_Create_roles_tables extends CI_Migration
 {
 
-public function up()
-{
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'role_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'created_at' => array(
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+            ),
+            'modified_at' => array(
+                'type' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+            ),
 
-}
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('roles');
+    }
 
-public function down()
-{
-
-}
+    public function down()
+    {
+        $this->dbforge->drop_table('roles');
+    }
 }

782e559d3ce09a5f92fb4c86e690b877d1cc45b1
Edgar Eldy <ed.eldy21@live.com>
2021-08-15 22:56:10 +0200

    Get all roles

--- application/models/Role.php
+++ application/models/Role.php
@@ -1,16 +1,26 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Role extends CI_Model {
-
-//Constructor
-public function __construct()
+class Role extends CI_Model
 {
-    parent::__construct();
-    //Do your magic here
-}
 
+    //Constructor
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+    // Get all roles
+    public function getRoles()
+    {
+        $this->db->select('*');
+        $this->db->from('roles');
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
 }
 
 /* End of file Role.php */

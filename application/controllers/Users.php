2b865c7be49eeb0270c8436671e5be5e9514bb51
Edgar Eldy <ed.eldy21@live.com>
2021-08-17 22:12:02 +0200

    Add Users controller

---
+++ application/controllers/Users.php
@@ -0,0 +1,22 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    //Constructor
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+    
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file User.php */

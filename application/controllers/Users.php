88eaa510d669a1012df058af1aaa0289f7893461
Edgar Eldy <ed.eldy21@live.com>
2021-08-18 22:36:00 +0200

    Getting users with their respective roles method

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,28 +1,31 @@
 <?php
 
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Users extends CI_Controller {
+class Users extends CI_Controller
+{
 
-    //Constructor
-    public function __construct()
-    {
-        parent::__construct();
+	//Constructor
+	public function __construct()
+	{
+		parent::__construct();
 
-        //Load Role Model
-        $this->load->model('Role');
-        
-        //Load User Model
-        $this->load->model('User');
-        
-        //Do your magic here
-    }
-    
-    public function index()
-    {
-        
-    }
+		//Load Role Model
+		$this->load->model('Role');
+
+		//Load User Model
+		$this->load->model('User');
+
+		//Do your magic here
+	}
+
+	//Getting users with their respective roles method
+	public function index()
+	{
+		$data['users'] = $this->User->getUsers();
+		return $this->render('templates/default', 'content', 'users/index', $data);
+	}
 
 }
 

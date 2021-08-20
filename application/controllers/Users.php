07eb8968017f166989cf66e1fb43212e6630ffe1
Edgar Eldy <ed.eldy21@live.com>
2021-08-20 11:14:46 +0200

    Insert a new user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -27,6 +27,33 @@ class Users extends CI_Controller
 		return $this->render('templates/default', 'content', 'users/index', $data);
 	}
 
+	public function add()
+	{
+		//Get role names
+		$data['roles'] = $this->Role->getRoles();
+
+		$this->form_validation->set_rules('first_name', 'First name', 'required');
+		$this->form_validation->set_rules('last_name', 'Last name', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[customers.email]');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('confirm_password', 'Please confirm your password', 'trim|required|matches[password]');
+
+		if ($this->form_validation->run() === FALSE) {
+			return $this->render('templates/default', 'content', 'users/add', $data);
+		} else {
+
+			//Save user
+			$this->User->add();
+
+			//Set flash message
+			$this->session->set_flashdata('user_saved', 'User has been saved successfully !');
+
+			//Load users/index view
+			return redirect('users');
+		}
+	}
+
+
 }
 
 /* End of file User.php */
